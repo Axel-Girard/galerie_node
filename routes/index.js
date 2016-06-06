9a5047e04a782695ba4fd4356fc9792ff1b90e32
agirard <axel.girard@trsb.net>
2016-06-06 14:47:22 +0200

    Ajout d'un test de download, de la récupération des arguments ainsi que d'un peu de sécurité

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/home', function(req, res, next) {
+  var max = process.argv.slice(2).length;
+  console.log('total argu : ' + max);
+  res.render('index', { title: 'Express', argc: max, argv: process.argv });
 });
 
 // test, faut bien s'amuser
 router.get('/bp', function(req, res, next) {
-  res.render('index', { title: 'MY page, bitch', image: '00.jpg'});
+  res.render('bp', { title: 'my page', argv: process.argv});
+});
+
+// test, un brin plus poussé; ça reste très simple faut pas déconner
+router.get('/bp', function(req, res, next) {
+  res.render('bp', { title: 'my page', argv: process.argv});
+});
+
+router.get('/dw', function (req, res) {
+  res.download('app.js');
 });
 
 module.exports = router;

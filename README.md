0dc4e3fe69b55736549fd828c87511183d7c26ae
agirard <axel.girard@trsb.net>
2016-06-08 11:59:32 +0200

    lecture des images à partir des arguments

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 # galerie_node
 
-Une galerie fait en JavaScript avec node.
+Une galerie d'image faite en JavaScript avec node.
 
 En construction.
 
 ![alt tag](https://media.giphy.com/media/UBSQSpyFUSgcE/giphy.gif)
+
+Pour installer le projet :
+
+git clone https://github.com/Axel-Girard/galerie_node.git
+cd galerie_node
+npm install
+npm start
+
+Mais y'a pas de mise en forme donc pas de magie.
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var fs = require('fs');
 var path = require('path');
+var http = require('http');
+var url = require('url');
+var colors = require('colors/safe');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var max = process.argv.slice(2).length;
-  console.log('total argu : ' + max);
-  res.render('index', { title: 'Express', argc: max, argv: process.argv });
+  console.log(colors.random('total argu : ' + max));
+  res.render('index', {
+    title: 'Express',
+    argc: max, argv: process.argv
+  });
 });
 
 // test, faut bien s'amuser
@@ -18,33 +24,50 @@ router.get('/bp', function(req, res, next) {
   });
 });
 
+// download of the fille app.js, porque ? porque not !
 router.get('/dw', function (req, res) {
   res.download('app.js');
 });
 
-// lire le contenu d'un répertoire
-router.get('/explorer', function(req, res, next) {
-  var p = "../"
-  fs.readdir(p, function (err, files) {
-      if (err) {
-          throw err;
-      }
+// lire une image n'importe où sur le serveur
+router.get('/img/*', function(req, res, next) {
+  var request = url.parse(req.url, true);
+  var path = request.pathname.substring(5);
+  path = decodeURIComponent(path);
 
-      files.map(function (file) {
-          return path.join(p, file);
-      }).filter(function (file) {
-          return fs.statSync(file).isFile();
-      }).forEach(function (file) {
-          console.log("%s (%s)", file, path.extname(file));
-      });
+  var file = fs.readFileSync(path);
+  res.writeHead(200, {'Content-Type': 'image/gif' });
+  res.end(file, 'binary');
+});
+
+// lire les images des path passé dans les arguments
+router.get('/image', function(req, res, next) {
+  var elem = process.argv.slice(2);
+  var images = [];
+  elem.forEach(function (name) {
+    images = images.concat(getImages(name));
+  });
+  res.render('image', {
+    img: images
   });
 });
 
-// lire le contenu d'un répertoire
-router.get('/image', function(req, res, next) {
-   var img = fs.readFileSync(process.argv[2]);
-   res.writeHead(200, {'Content-Type': 'image/gif' });
-   res.end(img, 'binary');
-});
+// return all file path from a path, recursivly
+function getImages(path){
+  var ret = [];
+
+  if (fs.lstatSync(path).isDirectory()) {
+    console.log(colors.white(colors.trap('Here we go!')));
+    var innerFile = [];
+    fs.readdirSync(path).forEach(function (name) {
+      console.log(colors.gray(path+'/'+name));
+      innerFile = innerFile.concat(getImages(path+'/'+name));
+    });
+    ret = ret.concat(innerFile);
+    return ret;
+  } else {
+    return path;
+  }
+}
 
 module.exports = router;

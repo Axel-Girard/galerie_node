645a2e265fb7444aa5e8e8c831d1cdef3af59bac
tryHardeur <axel.girard@u-psud.fr>
2016-06-12 12:10:26 +0200

    Passage de Jade à Pug

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(__dirname + '/public/img/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
